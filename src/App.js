358cedab8e8bbc1ec116ed95cd8723f43f89656e
Mohanish-5744 <mohakhamb76@gmail.com>
2025-05-09 12:44:57 +0530

    build fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ApiForm from './Components/ApiForm';
-// import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <ApiForm />
-      {/* <Footer /> */}
     </div>
   );
 }
